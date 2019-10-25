b020e68cb5376fe08f9290b7e8eb56f5604608e7
Spyryls <yr410ad@gmail.com>
2019-10-24 22:57:24 -0500

    Fixed Parts 1-4

---
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orbit-counts',
+  templateUrl: './orbit-counts.component.html',
+  styleUrls: ['./orbit-counts.component.css']
+})
+export class OrbitCountsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
