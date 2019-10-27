20e2815b9cb2a2a707cdf4ffe7336033a8b7f3dc
Spyryls <yr410ad@gmail.com>
2019-10-27 13:56:10 -0500

    Completed Part 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Satellite } from './satellite';
+import { JsonPipe } from '@angular/common';
+import { SourceListMap } from 'source-list-map';
 
 @Component({
   selector: 'app-root',
@@ -8,19 +10,29 @@ import { Satellite } from './satellite';
 })
 export class AppComponent {
   title = 'orbit-report';
-  sourceList: Satellite[] = []; 
+  sourceList: Satellite[] = [];
 
   constructor() {
-    this.sourceList = [
-       new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-       new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-       new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-       new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-       new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-    ];
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+    window.fetch(satellitesUrl).then(function (response) {
+      response.json().then(function (data) {
+
+        let fetchedSatellites = data.satellites;
+        for (let i = 0; i < fetchedSatellites.length; i++) {
+          let satellite = {
+            name: <string> fetchedSatellites[i].name,
+            type: <string> fetchedSatellites[i].type,
+            launchDate: <string> fetchedSatellites[i].launchDate,
+            orbitType: <string> fetchedSatellites[i].orbitType,
+            operational: <string> fetchedSatellites[i].operational
+          }
+          this.sourceList.push(satellite);
+        }
+        
+      }.bind(this));
+    }.bind(this));
+
   }
 }
-
-
-
-
