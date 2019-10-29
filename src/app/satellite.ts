e3cea065dfeaaf7a2a34c39ec05fea48dbe357a6
Spyryls <yr410ad@gmail.com>
2019-10-28 22:10:48 -0500

    Complete Except for shouldShowWarning

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -5,20 +5,15 @@ export class Satellite {
     operational: boolean;
     launchDate: string;
 
-    shouldShowWarning(): boolean {
-        return this.type.toLowerCase() === "space debris";
-        //  if (this.type === "space debris") {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-    }
-
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
         this.name = name;
         this.type = type;
         this.launchDate = launchDate;
         this.orbitType = orbitType;
         this.operational = operational;
+    }
+
+    shouldShowWarning(): boolean {
+        return this.type.toLowerCase() === "space debris";
     }
 }
